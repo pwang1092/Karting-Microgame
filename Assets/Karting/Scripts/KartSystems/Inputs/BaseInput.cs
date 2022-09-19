47af852f77e355f2f3f03418fa7f74c8dbda2fdf
plwdr <plwdremingtom@gmail.com>
2022-09-18 19:27:48 -0700

    Initial commit

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/BaseInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems
+{
+    public struct InputData
+    {
+        /// </summary>
+        public bool Accelerate;
+        //public bool BoostAccelerate;
+        public bool Brake;
+        public float TurnInput;
+    }
+
+    public interface IInput
+    {
+        InputData GenerateInput();
+    }
+
+    public abstract class BaseInput : MonoBehaviour, IInput
+    {
+        /// <summary>
+        /// Override this function to generate an XY input that can be used to steer and control the car.
+        /// </summary>
+        public abstract InputData GenerateInput();
+    }
+}
---
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public string TurnInputName = "Horizontal";
+        public string AccelerateButtonName = "Accelerate";
+        //public string BoostAccelerateName = "BoostAccelerate";
+        public string BrakeButtonName = "Brake";
+
+        public override InputData GenerateInput() {
+            return new InputData
+            {
+                Accelerate = Input.GetButton(AccelerateButtonName),
+               // BoostAccelerate = Input.GetButton(BoostAccelerateName), 
+                Brake = Input.GetButton(BrakeButtonName),
+                TurnInput = Input.GetAxis("Horizontal")
+            };
+        }
+    }
+}
